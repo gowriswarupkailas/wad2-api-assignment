82ed7e6dbd4790385a534f30f472d1afcc592072
Gowriswarup Kailas Perumal <kgswarup19@gmail.com>
2021-12-04 01:17:35 +0000

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -32,6 +32,13 @@ const MovieListPage = (props) => {
     else setGenreFilter(value);
   };
 
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -60,8 +67,8 @@ const MovieListPage = (props) => {
       genreFilter={genreFilter}
     />
         </Grid>
-        <MovieList movies={displayedMovies} />
-      </Grid>
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
+        </Grid>
     </Grid>
   );
 };

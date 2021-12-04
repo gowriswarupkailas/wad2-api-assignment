5f47736357ab235567fb4c9f61fa8531e8b29814
Gowriswarup Kailas Perumal <kgswarup19@gmail.com>
2021-12-04 01:24:49 +0000

    Refactor: Template list page; HTTP API calls centralized.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,36 +1,10 @@
-import React, { useState, useEffect } from "react";  // Changed
-import Header from "../components/headerMovieList";
-import FilterCard from "../components/filterMoviesCard";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import MovieList from "../components/movieList";
+import React, { useState, useEffect } from "react";
+import PageTemplate from '../components/templateMovieListPage'
 
-const useStyles = makeStyles({
-  root: {
-    padding: "20px",
-  },
-});
-
-const MovieListPage = (props) => {
-  const classes = useStyles();
+const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
-  const [nameFilter, setNameFilter] = useState("");
-  const [genreFilter, setGenreFilter] = useState("0");
-
-  const genreId = Number(genreFilter);
-
-  let displayedMovies = movies
-    .filter((m) => {
-      return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
-    })
-    .filter((m) => {
-      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
-    });
-
-  const handleChange = (type, value) => {
-    if (type === "name") setNameFilter(value);
-    else setGenreFilter(value);
-  };
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
 
   const addToFavorites = (movieId) => {
     const updatedMovies = movies.map((m) =>
@@ -45,7 +19,6 @@ const MovieListPage = (props) => {
     )
       .then((res) => res.json())
       .then((json) => {
-        // console.log(json);
         return json.results;
       })
       .then((movies) => {
@@ -55,21 +28,11 @@ const MovieListPage = (props) => {
   }, []);
 
   return (
-    <Grid container className={classes.root}>
-      <Grid item xs={12}>
-        <Header title={"Home Page"} />
-      </Grid>
-      <Grid item container spacing={5}>
-        <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
-        <FilterCard
-      onUserInput={handleChange}
-      titleFilter={nameFilter}
-      genreFilter={genreFilter}
+    <PageTemplate
+      title='Discover Movies'
+      movies={movies}
+      selectFavorite={addToFavorites}
     />
-        </Grid>
-        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
-        </Grid>
-    </Grid>
   );
 };
-export default MovieListPage;
+export default HomePage;
